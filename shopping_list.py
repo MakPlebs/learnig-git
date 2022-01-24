f7bec1a51975c4e721c258a78e2557c51143b2e0
Maciek Watrobski <maciek.watrobski@gmail.com>
2022-01-24 21:58:24 +0100

    Cut last bracket in code

--- shopping_list.py
+++ shopping_list.py
@@ -3,3 +3,4 @@ zakupy={ "piekarnia": ["chleb", "paczek", "bulki"], "warzywniak": ["marchew", "s
 print("Lista zakupów")
 for sklep, produkty in zakupy.items():
     print("Idę do,", (sklep.capitalize()), "kupuję tu następujące rzeczy:", [i.title() for i in produkty])
+print("W sumie kupuję", ((sum([len(zakupy[x]) for x in zakupy if isinstance(zakupy[x], list)]))), "produktów."
