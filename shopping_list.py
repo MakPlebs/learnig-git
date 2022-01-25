cc6704863a406b74dc25cf0b67e0df02be7004bc
MakPlebs <98196169+MakPlebs@users.noreply.github.com>
2022-01-25 14:45:41 +0100

    changes-in-shopping-list

--- shopping_list.py
+++ shopping_list.py
@@ -4,3 +4,4 @@ print("Lista zakupów")
 for sklep, produkty in zakupy.items():
     print("Idę do,", (sklep.capitalize()), "kupuję tu następujące rzeczy:", [i.title() for i in produkty])
 print("W sumie kupuję", ((sum([len(zakupy[x]) for x in zakupy if isinstance(zakupy[x], list)]))), "produktów.")
+print("zmainy, próby w git-cie)
